204550072fa39741f2b0bc1b84482e6104a48772
Bryan <r0889617@student.thomasmore.be>
2023-03-20 11:27:28 +0100

    progress

--- code/mobileapp/lib/components/home_buttons.dart
+++ code/mobileapp/lib/components/home_buttons.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeButtons extends StatelessWidget {
-  const HomeButtons({
-    required this.list,
-    super.key
-    });
+  const HomeButtons({required this.list, super.key});
 
-    final List list;
+  final List list;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Container homeButton() {
+      return Container(
+        margin: const EdgeInsets.all(0.0),
+        padding: const EdgeInsets.all(18.0),
+        decoration: BoxDecoration(
+            color: Colors.white.withAlpha(64),
+            borderRadius: const BorderRadius.all(Radius.circular(30))),
+        child: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Column(
+            children: [
+              const FaIcon(
+                FontAwesomeIcons.newspaper,
+                color: Color(0xBBFFFFFF),
+                size: 82,
+              ),
+              const SizedBox(height: 15), // padding tussen icon en tekst
+              Text(
+                "name",
+                style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                    shadows: [
+                      const Shadow(
+                        blurRadius: 5,
+                        color: Colors.black45,
+                        offset: Offset(0, 2),
+                      )
+                    ]),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      children: list.asMap().entries.map((info) {
+        return homeButton();
+      }).toList(),
+    );
   }
 }
--- code/mobileapp/lib/pages/home.dart
+++ code/mobileapp/lib/pages/home.dart
@@ -18,36 +18,47 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+        final double screenHeight = MediaQuery.of(context).size.height;
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Home',
-      home: Scaffold(
-          extendBodyBehindAppBar: true,
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            title: const Text(
-              'De Waaiburg',
-              style: TextStyle(
-                color: Color(0xFFF3D015),
-                shadows: [Shadow(color: Colors.black, offset: Offset(0, 3), blurRadius: 15)],
-                fontSize: 48,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-          ),
-          body: Container(
-            width: double.maxFinite,
-            height: double.maxFinite,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Color(0xFFF25B58), Color(0xFFF38E3B)],
-              ),
-            ),
-            child: HomeButtons(list: paginas),
-          )),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Home',
+        theme: ThemeData(fontFamily: 'Poppins'),
+        home: Scaffold(
+            body: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    colors: [Color(0xFFF25B58), Color(0xFFF38E3B)],
+                  ),
+                ),
+                child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin:
+                            EdgeInsets.only(top: screenHeight * 0.05, bottom: screenHeight * 0.05),
+                          child: const Center(
+                            child: Text(
+                              'De Waaiburg VZW',
+                              style: TextStyle(
+                                color: Color(0xFFF3D015),
+                                shadows: [
+                                  Shadow(
+                                      color: Colors.black45,
+                                      offset: Offset(0, 3),
+                                      blurRadius: 15)
+                                ],
+                                fontSize: 48,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                          ),
+                        ),
+                        HomeButtons(list: paginas),
+                      ],
+                    )))));
   }
 }
