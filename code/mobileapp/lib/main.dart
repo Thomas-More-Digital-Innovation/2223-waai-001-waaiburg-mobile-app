9e2dfb9aa331e737ce310dae4d408942f4595768
SiebeCamerman <siebecamerman@gmail.com>
2023-03-22 10:10:14 +0100

    alles kapot

--- code/mobileapp/lib/main.dart
+++ code/mobileapp/lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:mobileapp/pages/info_segments.dart';
 import 'package:mobileapp/pages/infotest.dart';
+import 'package:mobileapp/pages/info_contents.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(fontFamily: 'Poppins'),
-    initialRoute: '/',
+    initialRoute: '/infocontent',
     routes: {
-      '/': (context) => const InfoSegments(),
+      '/infosegment': (context) => const InfoSegments(
+            sectionId: 1,
+          ),
+      '/infocontent': (context) => const InfoContents(
+            infoId: 11,
+          ),
       '/infotest': (context) => const InfoTest(),
       // '/HomeScreen': (context) => Home(),
     },
