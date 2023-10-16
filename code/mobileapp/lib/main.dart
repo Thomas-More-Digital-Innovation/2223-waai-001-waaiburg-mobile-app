90c23d7a331c2dca5f08f812cfe9f98d96d0daa2
Bryan <r0889617@student.thomasmore.be>
2023-10-16 14:56:02 +0200

    wip character

--- code/mobileapp/lib/main.dart
+++ code/mobileapp/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:mobileapp/pages/info_content_select.dart';
 import 'package:mobileapp/pages/info_segments.dart';
 import 'package:mobileapp/pages/info_contents.dart';
 import 'package:mobileapp/pages/news.dart';
+import 'package:mobileapp/pages/tree_home.dart';
 
 /// The website screen
 /// This screen is a webview
@@ -12,7 +13,7 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(fontFamily: 'Epilogue'),
-    initialRoute: "/home",
+    initialRoute: "/treehome",
     routes: {
       '/login': (context) => const LoginPage(),
       '/infosegment': (context) => const InfoSegments(),
@@ -20,6 +21,7 @@ void main() {
       '/infocontentselect': (context) => const InfoContentSelected(),
       '/home': (context) => const Home(),
       '/news': (context) => const News(),
+      '/treehome': (context) => const TreeHome(),
     },
   ));
 }
---
+++ code/mobileapp/lib/pages/tree_home.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class TreeHome extends StatelessWidget {
+  const TreeHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background image (tree)
+          Image.asset(
+            'assets/tree.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          // Character image (smaller and in front)
+          Positioned(
+            bottom: 0, // Adjust the position as needed
+            right: -80, // Adjust the position as needed
+            child: Image.asset(
+              'assets/character.png',
+              width: 400, // Adjust the size as needed
+              height: 500, // Adjust the size as needed
+            ),
+          ),
+          // Speech Bubble
+          Positioned(
+            bottom: 60, // Adjust the position as needed
+            right: 20, // Adjust the position as needed
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Text(
+                    'Hello!',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 0,
+                  height: 0,
+                  child: CustomPaint(
+                    size: const Size(20, 20), // Size of the arrow
+                    painter: TriangleArrowPainter(Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TriangleArrowPainter extends CustomPainter {
+  final Color color;
+
+  TriangleArrowPainter(this.color);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.fill;
+
+    final path = Path();
+    path.moveTo(0, size.height);
+    path.lineTo(size.width / 2, 0);
+    path.lineTo(size.width, size.height);
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
